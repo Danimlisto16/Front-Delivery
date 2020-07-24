8413bb85211bff9cab277df2d051ec60a71f2461
mattgr99 <jmgranja1@espe.edu.ec>
2020-07-24 10:11:58 -0500

    Correccion archivos sobreescritos

--- FrontDelivery/src/app/app-routing.module.ts
+++ FrontDelivery/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { ClienteMainComponent } from './cliente-main/cliente-main.component';
 import { ClienteFormComponent } from './cliente-main/cliente-form/cliente-form.component';
 import { ClienteListComponent } from './cliente-main/cliente-list/cliente-list.component';
+import { ProductoListComponent } from './components/producto-main/producto-list/producto-list.component';
+import { ProductoFormComponent } from './components/producto-main/producto-form/producto-form.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
   { path: 'cliente', component: ClienteMainComponent },
   { path: 'cliente/Form', component: ClienteFormComponent },
-  { path: 'cliente/List', component: ClienteListComponent }
+  { path: 'cliente/List', component: ClienteListComponent },
+  {path: 'productos', component: ProductoListComponent},
+  {path: 'productos/add', component:ProductoFormComponent},
+  {path: 'productos/edit/:id', component: ProductoFormComponent}
   
 
 ];
